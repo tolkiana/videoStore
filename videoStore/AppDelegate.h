f5bfa540e89f503a5a7b199265c3aa10049a47da
Nelida Velazquez <tolkiana@gmail.com>
2016-01-05 22:48:25 -0500

    Initial Commit

---
+++ videoStore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  videoStore
+//
+//  Created by Nelida Velazquez on 1/5/16.
+//  Copyright © 2016 Tolkiana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ videoStore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  videoStore
+//
+//  Created by Nelida Velazquez on 1/5/16.
+//  Copyright © 2016 Tolkiana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
