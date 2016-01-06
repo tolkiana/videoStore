f8b765d83d1b919541b8639476f32c14b826911a
Nelida Velazquez <tolkiana@gmail.com>
2016-01-05 23:07:17 -0500

    Create methods to set and get priceCode and title

--- videoStore/Movie.m
+++ videoStore/Movie.m
@@ -8,6 +8,13 @@
 
 #import "Movie.h"
 
+@interface Movie()
+
+@property (nonatomic, strong, readwrite) NSString *title;
+@property (nonatomic, readwrite) NSInteger priceCode;
+
+@end
+
 @implementation Movie
 
 - (id)initWithTitle:(NSString *)title andPriceCode:(NSInteger)priceCode{
@@ -18,6 +25,22 @@
         _priceCode = priceCode;
     }
     return self;
+}
+
+- (void)setMovieTitle:(NSString *)movieTitle{
+    _title = movieTitle;
+}
+
+- (NSString *)getMovieTitle{
+    return _title;
+}
+
+- (void)setMoviePrice:(NSInteger)price{
+    _priceCode = price;
+}
+
+- (NSInteger)getMoviePrice{
+    return _priceCode;
 }
 
 @end
