4637eb1ac7b796e6a7b1dc8a7debcf53a9c7102a
Nelida Velazquez <tolkiana@gmail.com>
2016-01-05 23:57:51 -0500

    Implementing this ugly statement method

--- videoStore/Customer.m
+++ videoStore/Customer.m
@@ -35,7 +35,51 @@
 }
 
 - (NSString *)statement{
-    NSString *result;
+    double totalAmount = 0;
+    int frequentRentedPoints = 0;
+    NSString *result = [NSString stringWithFormat:@"Rental Record for %@ \n", [self getName]];
+    
+    NSMutableArray *currentRentals = self.rentals;
+    
+    while (currentRentals > 0) {
+        double thisAmount = 0;
+        Rental *each = self.rentals.lastObject;
+        
+        switch ([[each getMovie] getMoviePrice]) {
+            case REGULAR:
+                thisAmount += 2;
+                if ([each getDaysRented] > 2)
+                    thisAmount += ([each getDaysRented] - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                thisAmount += [each getDaysRented] * 3;
+                break;
+            case CHILDRENS:
+                thisAmount += 1.5;
+                if ([each getDaysRented] > 3)
+                    thisAmount += ([each getDaysRented] - 3) * 1.5;
+            default:
+                break;
+        }
+        
+        // Add frequent renter points
+        frequentRentedPoints ++;
+        // Add bonus for a two day new release rental
+        if ([[each getMovie] getMoviePrice] == NEW_RELEASE && [each getDaysRented] > 1)
+            frequentRentedPoints ++;
+        
+        // Show figures for this rental
+        result = [NSString stringWithFormat:@"%@ %@ %.2f \n", [result copy], [[each getMovie] getMovieTitle], thisAmount];
+        totalAmount += thisAmount;
+        
+        // Remove the rental
+        [currentRentals removeLastObject];
+    }
+    
+    // Add footer lines
+    result = [NSString stringWithFormat:@"%@ Amount owed is %.2f \n", [result copy], totalAmount];
+    result = [NSString stringWithFormat:@"%@ You earned %d \n", [result copy], frequentRentedPoints];
+    
     return result;
 }
 
